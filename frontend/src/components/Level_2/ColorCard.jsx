a045c627c1d021170671ce022b6880a235ea7678
Nabillera <vatcharadzenini@gmail.com>
2026-05-03 18:44:08 +0400

    Add copy to clipboard functionality to Level 2

--- frontend/src/components/Level_2/ColorCard.jsx
+++ frontend/src/components/Level_2/ColorCard.jsx
@@ -1,23 +1,53 @@
 import CopyButtonWhite from "../../assets/copy-white.png";
 import CopyButtonBlack from "../../assets/copy-black.png";
+import { useState } from "react";
 
 export function ColorCard({ color, name, isNeon }) {
+  const [isCopied, setIsCopied] = useState(false);
+
+  const handleCopyHex = () => {
+    navigator.clipboard.writeText(color);
+    setIsCopied((prevValue) => !prevValue);
+    setTimeout(() => {
+      setIsCopied((prevValue) => !prevValue);
+    }, 1000);
+  };
+
   return (
     <div>
       <div
         style={{ backgroundColor: color }}
-        className="rounded-t-[10px] w-[200px] h-[160px]"
-      ></div>
+        className="flex justify-end items-end rounded-t-[10px] w-[200px] h-[160px]"
+      >
+        {isCopied && (
+          <div
+            style={{
+              backgroundColor: isNeon ? "black" : "white",
+              color: isNeon ? "white" : "black",
+              borderColor: isNeon ? "white" : "black",
+            }}
+            className="font-inconsolata border-1 px-[7px]"
+          >
+            Copied!
+          </div>
+        )}
+      </div>
       <div
         style={{ backgroundColor: isNeon ? "black" : "white" }}
         className="flex rounded-b-[10px] justify-between p-[10px]"
       >
-        <div style={{color: isNeon ? "white" : "black"}} className="flex flex-col w-[80%] h-[70px] font-inconsolata ">
+        <div
+          style={{ color: isNeon ? "white" : "black" }}
+          className="flex flex-col w-[80%] h-[70px] font-inconsolata "
+        >
           <span>{color}</span>
           <span>{name}</span>
         </div>
         <div className="h-fit cursor-pointer">
-          <img src={isNeon ? CopyButtonWhite : CopyButtonBlack} />
+          <img
+            src={isNeon ? CopyButtonWhite : CopyButtonBlack}
+            onClick={handleCopyHex}
+          />
         </div>
       </div>
     </div>
