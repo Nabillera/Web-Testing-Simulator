b0473f6fbf1cb08d9d7a514c91ae65e1285cd590
Nabillera <vatcharadzenini@gmail.com>
2026-05-16 20:31:07 +0400

    Level 4: Add error simulation on theme selection

--- frontend/src/components/Level_4/LevelFour.jsx
+++ frontend/src/components/Level_4/LevelFour.jsx
@@ -40,43 +40,62 @@ export function LevelFour() {
       <h2 className="font-pixelify font-bold text-[40px]">
         Level 4: No, I do NOT want notifications
       </h2>
-      <div className="flex flex-col items-start w-full gap-y-[10px]">
-        <div className="flex gap-x-[30px]">
-          <img src={ProfilePicture} className="rounded-full w-[150px]" />
-          <div className="flex flex-col justify-center gap-y-[10px]">
-            <div className="flex gap-x-[30px] items-center">
-              <span className="font-pixelify text-[28px] font-bold">
-                Manuelo Costa
-              </span>
-              <span className="font-inconsolata font-bold text-[#0A5598] text-[20px] cursor-pointer hover:text-[#0A5598]/50">
-                Edit
+      {selectedTheme == "surprise" ? (
+        <div className="bg-[#FFFFFF] w-full h-full font-inconsolata flex flex-col items-center justify-center">
+          <span className="text-[20px] font-bold">500 Internal Server Error</span>
+          <span>Something went wrong :(</span>
+        </div>
+      ) : (
+        <div className="flex flex-col items-start w-full gap-y-[10px]">
+          <div className="flex gap-x-[30px]">
+            <img src={ProfilePicture} className="rounded-full w-[150px]" />
+            <div className="flex flex-col justify-center gap-y-[10px]">
+              <div className="flex gap-x-[30px] items-center">
+                <span className="font-pixelify text-[28px] font-bold">
+                  Manuelo Costa
+                </span>
+                <span className="font-inconsolata font-bold text-[#0A5598] text-[20px] cursor-pointer hover:text-[#0A5598]/50">
+                  Edit
+                </span>
+              </div>
+              <span className="font-inconsolata text-[20px]">
+                mcosta@kanudos.com
               </span>
             </div>
-            <span className="font-inconsolata text-[20px]">
-              mcosta@kanudos.com
-            </span>
           </div>
-        </div>
-        <div className="w-full flex justify-between">
-          <div className="w-[30%]">
-            <NotificationsSection toggleOn={toggleOn} onToggle={handleToggle} />
-            <ThemeSection
-              selectedTheme={selectedTheme}
-              onSetTheme={handleSetTheme}
-            />
-          </div>
-          <div className="w-[30%] flex flex-col justify-between">
-            <AccountInfoSection />
-            <div className="flex flex-col items-center">
-              <Button width="50%" backColor="#C1011A" textColor="#F2F2F2" textSize="24px">Save Changes</Button>
-              <span className="font-inconsolata text-[#0A5598] text-[22px] font-bold hover:text-[#0A5598]/50 cursor-pointer">Refresh Page</span>
+          <div className="w-full flex justify-between">
+            <div className="w-[30%]">
+              <NotificationsSection
+                toggleOn={toggleOn}
+                onToggle={handleToggle}
+              />
+              <ThemeSection
+                selectedTheme={selectedTheme}
+                onSetTheme={handleSetTheme}
+              />
+            </div>
+            <div className="w-[30%] flex flex-col justify-between">
+              <AccountInfoSection />
+              <div className="flex flex-col items-center">
+                <Button
+                  width="50%"
+                  backColor="#C1011A"
+                  textColor="#F2F2F2"
+                  textSize="24px"
+                >
+                  Save Changes
+                </Button>
+                <span className="font-inconsolata text-[#0A5598] text-[22px] font-bold hover:text-[#0A5598]/50 cursor-pointer">
+                  Refresh Page
+                </span>
+              </div>
+            </div>
+            <div className="w-[30%]">
+              <DisplaySection />
             </div>
           </div>
-          <div className="w-[30%]">
-            <DisplaySection />
-          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
